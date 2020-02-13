1dc8d0c1562e9614780c7fd8be648e29931143c5
jason <jlu787@outlook.com>
2020-02-14 11:30:26 +1300

    Basic Behaviour Tree Setup for Mud Monster

--- CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.cpp
+++ CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#include "E:/school/3rd year/Production/Proto1/CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "D:/Users/rhyan.mcl7998/Desktop/Proto1/CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> develop
+#include "D:/Users/guo.lu8015/Desktop/REclone Proto1 again/Proto1/CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.h"
--- CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.cpp.old
+++
@@ -1,1 +0,0 @@
-#include "D:/Users/guo.lu8015/Desktop/REclone Proto1/Proto1/CombatArena/Intermediate/Build/Win64/CombatArenaEditor/Development/Engine/SharedPCH.Engine.h"
