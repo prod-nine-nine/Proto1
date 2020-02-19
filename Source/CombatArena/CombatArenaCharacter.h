e35db76267497367034217f9609c80bce3329cdb
rhynz <rhynz@hotmail.co.nz>
2020-02-19 17:46:14 +1300

    gitflow-feature-stash: weaponHitbox

--- Source/CombatArena/CombatArenaCharacter.h
+++ Source/CombatArena/CombatArenaCharacter.h
@@ -20,6 +20,15 @@ class ACombatArenaCharacter : public ACharacter
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* FollowCamera;
 
+	UPROPERTY(EditAnywhere)
+		class USphereComponent* RHColl;
+
+	UPROPERTY(EditAnywhere)
+		class USphereComponent* LHColl;
+
+	UPROPERTY(EditAnywhere)
+		class UBoxComponent* SwordColl;
+
 public:
 	ACombatArenaCharacter();
 
